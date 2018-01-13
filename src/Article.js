3b0abf910d54de750c55c4038d6478666164a98a
Roman Belik <dabonka@yandex.ru>
2018-01-13 21:49:36 +0300

    add article

---
+++ src/Article.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Article(){
+    return(
+        <div>
+            <h3>title</h3>
+            <section>
+                Text
+            </section>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import {render} from 'react-dom'
+import Article from './Article'
 
 function HelloWorld(){
     return <h1>Hello World</h1>
 }
 
-render(<HelloWorld/>, document.getElementById('container'))
+render(<Article/>, document.getElementById('container'))
