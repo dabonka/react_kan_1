29644b4bbc788dc65d6968d16eae15293393acc3
Roman Belik <dabonka@yandex.ru>
2018-01-14 21:02:44 +0300

    add component

--- src/Article.js
+++ src/Article.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-export default function Article(props){
-const {article} = props
-    return(
-        <div>
-            <h3>{article.title}</h3>
-            <section>
-                {article.text}
-            </section>
-        </div>
-    )
+export default class Article extends Component {
+    render() {
+        const {article} = this.props
+        return(
+            <div>
+                <h3>{article.title}</h3>
+                <section>
+                    {article.text}
+                </section>
+            </div>
+        )
+    }
 }
