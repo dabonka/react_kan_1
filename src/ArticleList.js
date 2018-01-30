01693d17e62fa31c8a6b01fef68ff7a05371f21a
Roman Belik <dabonka@yandex.ru>
2018-01-30 14:36:26 +0300

    add acticle list

--- src/ArticleList.js
+++ src/ArticleList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Article from './Article'
+import Comment from './Comment'
 
 export default function ArticleList({articles}) {
     const articleElements = articles.map((article) => <li key = {article.id}><Article article = {article}/></li>)
